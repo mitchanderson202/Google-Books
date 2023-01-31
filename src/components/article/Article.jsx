624662b8dfbeb303f7f5bb354b926ab8c0002420
Mitchell Anderson <mitchellanderson202@hotmail.com>
2023-01-31 16:36:50 +1100

    updated some styling

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -6,9 +6,8 @@ const article = () => {
       <p>
         Search for a book using the Input and the Search Button. This will
         search through the Google Books API and render a maximum of
-        <span style={{ fontSize: "1.2em", fontWeight: "bold" }}> 40 books</span>
-        , keep this in mind if you're just browsing. Otherwise search directly
-        for a Title or Artist.
+        <span> 40 books</span>, keep this in mind if you're just browsing.
+        Otherwise search directly for a Title or Artist.
       </p>
     </div>
   );
